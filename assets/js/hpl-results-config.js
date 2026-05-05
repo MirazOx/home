d7a76d0715f4af433f851bfd3e37cc656c4a8c3f
MirazOx <miraz8395@gmail.com>
2026-05-05 17:36:49 +0600

    Update appsScriptUrl in HPL_RESULTS_CONFIG

--- assets/js/hpl-results-config.js
+++ assets/js/hpl-results-config.js
@@ -1,5 +1,5 @@
     window.HPL_RESULTS_CONFIG = {
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzfseCJXlpp3wuC8FqyOTNBQJVdnZEqLq7FUmNON6AZ5RsDdEKvdGuuTBpsKJWXIHPNog/exec',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzrARL-hYUI3qEnpfI3FSg3SMRy6_OVECzmU3DKkLUA4jAEKZChuYzuI8BRcHLU8hEWbg/exec',
 
   emailEndpoint: 'https://formsubmit.co/ajax/miraz8395@gmail.com',
 
