04342af86afb30d5a571da2db0efde77680f77da
MirazOx <miraz8395@gmail.com>
2026-05-22 09:12:43 +0600

    Delete js directory

--- js/hpl-results-config.js
+++
@@ -1,9 +0,0 @@
-    window.HPL_RESULTS_CONFIG = {
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzrARL-hYUI3qEnpfI3FSg3SMRy6_OVECzmU3DKkLUA4jAEKZChuYzuI8BRcHLU8hEWbg/exec',
-
-  emailEndpoint: 'https://formsubmit.co/ajax/miraz8395@gmail.com',
-
-  teacherEmail: 'miraz8395@gmail.com',
-
-  defaultSessionCode: ''
-};
